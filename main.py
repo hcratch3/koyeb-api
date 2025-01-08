a44c9d4596bc5c3b5b52ec94e2bd42c6cae78c44
hcratch3 <162408146+hcratch3@users.noreply.github.com>
2025-01-08 18:09:38 +0900

    update

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
 from fastapi import FastAPI
 from pydantic import BaseModel
+import smtplib
+from email.mime.text import MIMEText
+from email.utils import formatdate
+import os
 
 app = FastAPI()
 
@@ -23,3 +27,31 @@ def read_item(item_id: int, q: str = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+
+@app.get("/mail/")
+def read_item(to: str, subject: str, body: str = None):
+    if to == null:
+        return {"status":"400","msg":"'to' is null"}
+    if subject == null:
+        return {"status":"400","msg":"'subject' is null"}
+    if body == null:
+        bodyText = ""
+    if body != null:
+        bodyText = body
+    fromAddress = 'noreply.hcratch3@gmail.com'
+
+    smtpobj = smtplib.SMTP('smtp.gmail.com', 587)
+    smtpobj.starttls()
+    smtpobj.login(os.environ.get("sendAddress"), os.environ.get("password"))
+
+    msg = MIMEText(bodyText)
+    msg['Subject'] = subject
+    msg['From'] = fromAddress
+    msg['To'] = to
+    msg['Date'] = formatdate()
+
+    # 作成したメールを送信
+    smtpobj.send_message(msg)
+    smtpobj.close()
+
+    return {"status":"200","msg":"success"}
